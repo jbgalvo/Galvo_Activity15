d304cb162066b77e01de4a5a2fb5600e25fc1e19
Jb Galvo <jbmgalvo16@gmail.com>
2024-02-05 13:25:59 +0800

    Added Task 2: Variable Declaration and Data Types

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
 
-echo 'PHP Basics';
+  // Task 2: Variable Declaration and Data Types
+  // Declare three variables: one for a string, one for an integer, and one for a boolean value.
+  $fullname = 'Jb Galvo'; // String
+  $age = 25; // Integer
+  $isSingle = true ? 'Yes' : 'No'; // Boolean
+
+  // Print the values of these variables using echo or print.
+  echo "<b>Task 2: Variable Declaration and Data Types</b>";
+  echo "<br /><br />";
+  echo "My name is {$fullname}, {$age} years old.";
+  echo "<br />";
+  print "Are you single? {$isSingle}";
+  echo "<br /><br />";
+
+  
 
 ?>
